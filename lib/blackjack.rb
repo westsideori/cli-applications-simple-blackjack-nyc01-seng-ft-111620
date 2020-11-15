be78853649a5ec72012487bc550d30958168cc74
Ori Markowitz <ori.markowitz@gmail.com>
2020-11-15 23:30:23 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,55 @@
+require "pry"
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
+  total
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(num)
+  puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_one = deal_card
+  card_two = deal_card
+  card_hand = card_one + card_two
+  display_card_total(card_hand)
 end
 
-def hit?
-  # code hit? here
+def hit?(total)
+  card_total = total
+  prompt_user
+  user_input = get_user_input
+  while user_input != 'h' && user_input != 's' do
+    invalid_command
+    prompt_user
+    user_input = get_user_input
+  end
+  if user_input == 'h'
+    new_card = deal_card
+    card_total += new_card
+  end
+  card_total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +57,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  total = initial_round
+  until total > 21 do
+    total = hit?(total)
+    display_card_total(total)
+  end
+  end_game(total)
 end
     
